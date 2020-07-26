66f84c70960342589e0eca1c6320908fe370114e
Arek Kaczorowski <mail>
2020-07-26 18:49:59 +0200

    Add queue by height reconstruction solution

--- src/test/kotlin/QueueByHeightReconstructionTest.kt
+++ src/test/kotlin/QueueByHeightReconstructionTest.kt
@@ -14,25 +14,60 @@ class QueueByHeightReconstructionTest {
                 Entry(6, 1),
                 Entry(5, 2)
         )
+
+        val result = reconstructQueueByHeight(array)
+
+        result.forEach { print(it) }
     }
 
 
-    data class Entry(val height: Int, val personsInFront: Int)
+    private data class Entry(val height: Int, val personsInFront: Int)
 
-    fun reconstructQueueByHeight(entries: Array<Entry>) {
-        val sortedArray = sortArray(entries)
-        val result = mutableListOf
+    private fun reconstructQueueByHeight(entries: Array<Entry>): Array<Entry> {
+        val sortedArray = sortArray(entries, true)
+        val result = mutableListOf<Entry>()
+        result.addAll(sortedArray)
+        result.clear()
 
         for (entry in sortedArray) {
-
-
+            result.add(entry.personsInFront, entry)
         }
 
+        return result.toTypedArray()
+    }
+
+    private fun sortArray(entries: Array<Entry>, isInverted: Boolean): Array<Entry> {
+        return quickSort(entries, 0, entries.size - 1, isInverted)
+    }
+
+    private fun quickSort(entries: Array<Entry>, start: Int, end: Int, isInverted: Boolean): Array<Entry> {
+        if (end - start < 1) return entries
+
+        val pivot = divide(entries, start, end, isInverted)
+        quickSort(entries, 0, pivot, isInverted)
+        quickSort(entries, pivot + 1, end, isInverted)
+
+        return entries
 
     }
 
-    private fun sortArray(entries: Array<Entry>): Array<Entry> {
+    private fun divide(array: Array<Entry>, start: Int, end: Int, isInverted: Boolean): Int {
+        var pivot = start
+        val pivotEntry = array[pivot]
+        val pivotValue = pivotEntry.height
 
+        var currentPosition = start
+        for (i in start..end) {
+            if (array[i].height < pivotValue && isInverted.not() ||
+                    array[i].height > pivotValue && isInverted) {
+                var swapEntry = array[currentPosition]
+                array[currentPosition] = array[i]
+                array[i] = swapEntry
+                ++currentPosition
+            }
+        }
+
+        return currentPosition
     }
 
 }
